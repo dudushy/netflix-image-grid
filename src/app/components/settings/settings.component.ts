b33c9eeaaa11cbf1b8bda361744c51598db0b357
dudushy <eduardotalarico1337@gmail.com>
2023-04-20 11:21:23 -0300

    Create settings.component.ts

---
+++ src/app/components/settings/settings.component.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { AppComponent } from '../../app.component';
+import { DbService } from '../../services/db/db.service';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.scss']
+})
+export class SettingsComponent implements OnInit {
+  title = 'SettingsComponent';
+
+  constructor(
+    public db: DbService,
+    private cdr: ChangeDetectorRef,
+    public app: AppComponent
+  ) {
+    console.log(`[${this.title}#constructor]`);
+  }
+
+  ngOnInit(): void {
+    console.log(`[${this.title}#ngOnInit]`);
+  }
+
+  updateView() {
+    console.log(`[${this.title}#updateView]`);
+
+    this.cdr.detectChanges;
+    this.app.updateView(this.title);
+  }
+
+  redirectTo(url: any) {
+    this.app.redirectTo(url, this.title);
+
+    this.updateView();
+  }
+}
